3b566fd168d3c417f672729765fce5c18ff55ddb
arnojobdomain <68260504+arnojobdomain@users.noreply.github.com>
2020-09-06 10:49:29 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,15 +1,18 @@
 (function(w) {
     function enterLine() {
         document.getElementById("btnEnter1").onclick = function() {
-            window.open('https://www.haoda1.net', "_blank");
-        }
-        document.getElementById("btnEnter2").onclick = function() {
             window.open('https://www.haoda2.net', "_blank");
         }
-        document.getElementById("btnEnter3").onclick = function() {
+        document.getElementById("btnEnter2").onclick = function() {
             window.open('https://www.haoda3.net', "_blank");
         }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('http://www.haoda4.net', "_blank");
+        }
         document.getElementById("btnEnter4").onclick = function() {
+            window.open('http://www.haoda5.net', "_blank");
+        }
+        document.getElementById("btnEnter5").onclick = function() {
             window.open('https://www.02298.com', "_blank");
         }
     }
